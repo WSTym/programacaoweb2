19a7139c8b0fbcaeee057dd49697854b9bc605a3
WSTym <serial.0.killer@gmail.com>
2019-10-27 15:28:14 -0300

    Otimização do layout

--- CRUD_Livraria/resources/views/index.blade.php
+++ CRUD_Livraria/resources/views/index.blade.php
@@ -2,36 +2,43 @@
 
 @section('content')
 
-    <h1>Catálogo de Livros</h1>
+    <div style="width:75%; margin:auto">
 
-    <div class="form-group">
-        <a href="{{route('books.create')}}" class="btn btn-primary btn-add"><span class="glyphicon glyphicon-plus"></span>  Cadastrar</a>
+        <h1>Catálogo de Livros</h1>
+
+        <div style="margin-bottom:2rem">
+            <a href="{{ route('books.create') }}" class="btn btn-primary btn-add"><span class="glyphicon glyphicon-plus"></span>  Cadastrar</a>
+        </div>
+
+        <div class="form-group">    
+            <table class="table table-bordered table-striped">
+                <tr>
+                    <th>Nome</th>
+                    <th>Autor</th>
+                    <th>Descrição</th> 
+                    <th class="text-center">Ações</th>
+                </tr>
+
+                @foreach ($books as $book)
+                <tr>
+                    <td>{{ $book->name }}</td>
+                    <td>{{ $book->author }}</td>
+                    <td>{{ $book->description }}</td> 
+                    <td class="text-center">
+                        <a href="{{ route('books.edit', $book->id) }}">
+                            <span class="glyphicon glyphicon-edit" style="margin-right:.5rem"></span>
+                        </a> 
+                        <a href="{{ route('books.show', $book->id) }}">
+                            <span class="glyphicon glyphicon-eye-open"></span>
+                        </a> 
+                    </td>
+                </tr>
+                @endforeach                
+            </table>
+        </div>
+
+        {!! $books -> links() !!}
+
     </div>
-    
-    <div class="form-group">    
-        <table class="table table-striped">
-            <tr>
-                <th>Nome</th>
-                <th>Autor</th>
-                <th>Descrição</th> 
-                <th>Ações</th>
-            </tr>
-
-            @foreach ($books as $book)
-            <tr>
-                <td>{{$book->name}}</td>
-                <td>{{$book->author}}</td>
-                <td>{{$book->description}}</td> 
-                <td>
-                    <a href="{{route('books.edit', $book->id)}}" class="edit actions"><span class="glyphicon glyphicon-edit"></span></a>
-                    <a href="{{route('books.show', $book->id)}}" class="view actions"><span class="glyphicon glyphicon-eye-open"></span></a>
-                </td>
-            </tr>
-            @endforeach 
-            
-        </table>
-    </div>
-
-    {!! $books -> links() !!}
 
 @endsection
--- CRUD_Livraria/resources/views/show.blade.php
+++ CRUD_Livraria/resources/views/show.blade.php
@@ -2,57 +2,62 @@
 
 @section('content')
 
-<h1 class="text-uppercase"><strong>{{$book->name}}</strong></h1>
+    <div style="width:75%; margin:auto">
 
-<div class="row">
-    <div class="col-md-3">
-        <p><img src="{{url("storage/img/$book->image")}}" alt="{{$book->name}}" class="img-thumbnail"></p>
-    </div>
-    <div class="container-fluid">
-        <div class="col-md-5">
-            <p><strong>Autor:</strong> {{$book->author}}</p>
-        </div>
-        <div class="col-md-5">
-            <p><strong>Descrição:</strong> {{$book->description}}</p>
-        </div>
-    </div>        
-</div>
+        <h1 class="text-uppercase"><strong>{{ $book->name }}</strong></h1>
 
-<div class="form-group">
-    <a href="{{route('books.index')}}" class="btn btn-primary"><span class="glyphicon glyphicon-chevron-left"></span>  Voltar</a>
-    <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#deleteModal"><span class="glyphicon glyphicon-remove"></span> Deletar livro</button>
-</div>
-
-@if (isset($errors) && count($errors) > 0)
-    <div class="alert alert-danger">
-        @foreach ($errors->all() as $error)
-            <p>{{ $error }}</p>
-        @endforeach
-    </div>
-    @endif
-
-<div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="deleteModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-danger" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="deleteModalLabel">Deletar livro</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
+        <div class="row">
+            <div class="col-md-3">
+                <img src="{{ url("storage/img/$book->image") }}" alt="{{ $book->name }}" class="img-thumbnail">
             </div>
-            <div class="modal-body">
-                <p>Tem certeza que deseja excluir o livro?</p>
-            </div>
-            <div class="modal-footer">
-                <form method="POST" action="{{route('books.destroy', $book->id)}}">
-                    {!! method_field('DELETE') !!}
-                    {!! csrf_field() !!}
-                    <button type="button" class="btn btn-primary" data-dismiss="modal">Cancelar</button>
-                    <button class="btn btn-danger "><span class="glyphicon glyphicon-remove"></span> Deletar livro</button>
-                </form>
+            <div class="col-md-6">
+                <p><strong>Autor:</strong> {{ $book->author }}</p>
+                <p><strong>Descrição:</strong> {{ $book->description }}</p>
+            </div>        
+        </div>
+
+        <div style="margin-top:2rem">
+            <a href="{{ route('books.index') }}" class="btn btn-primary">
+                <span class="glyphicon glyphicon-chevron-left"></span>  Voltar
+            </a>
+            <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#deleteModal">
+                <span class="glyphicon glyphicon-remove"></span> Excluir livro
+            </button>
+        </div>
+
+        <div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="deleteModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-danger" role="document">
+                <div class="modal-content text-center">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="deleteModalLabel">Excluir livro</h5>
+                    </div>
+                    <div class="modal-body">
+                        <h3>Tem certeza que deseja excluir o livro?</h3>
+                    </div>
+                    <div class="modal-footer">
+                        <form method="POST" action="{{ route('books.destroy', $book->id) }}">
+                            {!! method_field('DELETE') !!}
+                            {!! csrf_field() !!}
+                            <button type="button" class="btn btn-primary" data-dismiss="modal">
+                                <span class="glyphicon glyphicon-chevron-left"></span> Não, volte!
+                            </button>
+                            <button class="btn btn-success"><span class="glyphicon glyphicon-ok">
+                                </span> Sim, exclua!
+                            </button>
+                        </form>
+                    </div>
+                </div>
             </div>
         </div>
+
+        @if (isset($errors) && count($errors) > 0)
+            <div class="alert alert-danger">
+                @foreach ($errors->all() as $error)
+                    <p>{{ $error }}</p>
+                @endforeach
+            </div>
+        @endif
+
     </div>
-</div>
 
 @endsection 
