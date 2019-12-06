05bab01ef7e9f15089dc6f729521c0b78146685e
WSTym <serial.0.killer@gmail.com>
2019-12-06 14:00:43 -0300

    Correção de parâmetro

--- calculator/src/main/Calculator.vue
+++ calculator/src/main/Calculator.vue
@@ -60,7 +60,7 @@ export default {
                 this.values[1] = 0
                 this.displayValue = this.values[0]
                 this.operation = equals ? null : operation
-                this.current = equals ? 0 : 
+                this.current = equals ? 0 : 1
                 this.clearDisplay = !equals
             }
         },
